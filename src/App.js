d1fede28f8a46e593b2c946152e2a27a543b36ff
PRASHANT PARASHAR (CRGL-THIRDPARTY.COM) <prashant_parashar@crgl-thirdparty.com>
2021-03-27 03:40:25 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { Data } from "./Sound";
 
 function App() {
+  const [audio, setaudio] = useState(null);
+  const [letter, setLetter] = useState(null);
+  useEffect(() => {
+    window.addEventListener("keydown", (e) => {
+      const filteredData = Data.filter((item) => {
+        return item.letter === e.key || item.letter.toLowerCase() === e.key;
+      });
+      if (filteredData.length > 0) {
+        setaudio(new Audio(filteredData[0].src));
+        setLetter(filteredData[0].letter);
+      }
+    });
+  }, []);
+
+  useEffect(() => {
+    if (audio !== null) {
+      audio.play();
+    }
+  }, [audio]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {Data.map((item) => {
+        return (
+          <div
+            key={item.letter}
+            className={item.letter === letter ? "box1" : "box"}
+          >
+            <div className="letter">{item.letter}</div>
+            <div className="label">{item.label}</div>
+          </div>
+        );
+      })}
     </div>
   );
 }
---
+++ src/Sound.js
@@ -0,0 +1,21 @@
+import Clap from "./Asset/sounds/clap.wav";
+import Hihat from "./Asset/sounds/clap.wav";
+import Kick from "./Asset/sounds/kick.wav";
+import Openhat from "./Asset/sounds/openhat.wav";
+import Ride from "./Asset/sounds/ride.wav";
+import Snare from "./Asset/sounds/snare.wav";
+import Tink from "./Asset/sounds/tink.wav";
+import Tom from "./Asset/sounds/tom.wav";
+import Boom from "./Asset/sounds/boom.wav";
+
+export const Data = [
+  { letter: "A", label: "Boom", src: Boom },
+  { letter: "S", label: "Hihat", src: Hihat },
+  { letter: "D", label: "Openhat", src: Openhat },
+  { letter: "F", label: "Snare", src: Snare },
+  { letter: "G", label: "Tom", src: Tom },
+  { letter: "H", label: "Clap", src: Clap },
+  { letter: "J", label: "Kick", src: Kick },
+  { letter: "K", label: "Ride", src: Ride },
+  { letter: "L", label: "Tink", src: Tink },
+];
